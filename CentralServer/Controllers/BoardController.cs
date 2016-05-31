eb269627c35c327b93f2f523580f3bebe00b94df
ionutss <stoicaionut93@gmail.com>
2016-05-31 15:04:43 +0300

    board controller

--- CentralServer/Controllers/BoardController.cs
+++ CentralServer/Controllers/BoardController.cs
@@ -17,17 +17,46 @@ namespace CentralServer.Controllers
         [HttpPost]
         public void Post(HttpRequestMessage request)
         {
-            var definition = new { Name = "", Timestamp = "" };
+            var definition = new { type = "", room = "" , date = ""};
 
             string jsonString = request.Content.ReadAsStringAsync().Result;
 
             var result = JsonConvert.DeserializeAnonymousType(jsonString, definition);
 
-            System.Diagnostics.Debug.WriteLine("BPOST: " + result.Name);
+            System.Diagnostics.Debug.WriteLine("BPOST: " + result.room);
 
-            activity.BoardFact = result.Name;
+            if(result.type == "room")
+            {
+                switch (result.room)
+                {
+                    case "1":
+                        activity.BoardFact = "Hall";
+                        break;
+                    case "2":
+                        activity.BoardFact = "Kitchen";
+                        break;
+                    case "3":
+                        activity.BoardFact = "Bedroom";
+                        break;
+                    case "4":
+                        activity.BoardFact = "Bathroom";
+                        break;
+                }
+            }
+            else if(result.type == "bed")
+            {
+                if(result.room == "1")
+                {
+                    activity.BoardFact = "BedOn";
+                }
+                else
+                {
+                    //activity.BoardFact = "BedOff";
+                }
+            }
 
-            Console.WriteLine(result.Name);
+           // activity.BoardFact = result.Name;
+
         }
 
     }
